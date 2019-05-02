f3e2a3a6b62729d8bc2a92269060e2a2dba5e2b9
Shingirai Chanakira <sechanakira@yahoo.com>
2019-05-02 21:30:12 +0200

    Initial Commit

--- rsa-id-generator.js
+++ rsa-id-generator.js
@@ -14,13 +14,37 @@ const randomDOB = () => {
     return new Date(randomYear, randomMonth, randomDay);
 };
 
-const generateRSAID = (citizen = false, sex = 'male') => {
+const generateRSAID = (citizen = false, gender = 'male') => {
     let dob = randomDOB();
-    console.log(dob);
-    let id = dob.toLocaleDateString()
+    let sex = '8';
+    if(gender == 'female'){
+        sex = '9';
+    }
+    let values = [
+        dob.getFullYear().toString(),
+        dob.getMonth().toString().padStart(2,'0'),
+        dob.getDay().toString().padStart(2,'0'),
+        sex
+    ];
+    let id = values.map(e=>e).join('').toString();
     return id;
 };
 
-for(let i = 0; i<=10; i++){
+const calculateCheckDigit = (digitsAsString) => {
+    //const digits = ('' + dob + gender + sequence + citizenship + eight).split('').map(d => Number(d))
+    const digits = digitsAsString.replace(/\D/g, '').split('').map(d => Number(d));
+    const checkSum = digits.reverse().map((d, ix) => {
+        if (ix % 2 === 0) {
+            d *= 2;
+            if (d > 9) {
+                d -= 9
+            }
+        }
+        return d
+    }).reduce((memo, d) => memo += d, 0);
+    return checkSum * 9 % 10
+};
+
+for (let i = 0; i <= 10; i++) {
     console.log(generateRSAID());
 }
